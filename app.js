d7b4dfec8f883692385ab7913b3d84032765a12f
Abhishek Lal <abhisheklalnediya@gmail.com>
2021-09-30 07:04:26 +0530

    hevc

--- app.js
+++ app.js
@@ -1,15 +1,83 @@
-// 4K VOD
-// var manifestUri = `https://uselector.cdn.intigral.net/Titles/M0019480/M0019480.ism/manifest.mpd?filter=((type=="video"%26%26MaxHeight<=2160)%7C%7C(type=="audio")%7C%7C(type=="textstream"))&codec=hevc`;
-// var manifestUri = `https://uselector.cdn.intigral.net/Titles/M0019480/M0019480.ism/manifest.mpd`;
-// 1080 VOD
-var manifestUri = `https://uselector.cdn.intigral.net/Titles/M0012400/M0012400.ism/manifest.mpd`;
-// var manifestUri = `https://uselector.cdn.intigral.net/Titles/M0019020/M0019020.ism/manifest.mpd"`;
-// Live
-// var manifestUri = `="https://uselector.cdn.intigral.net/JAW1/JAW1.isml/manifest.mpd`;
+const uri = [
+  // 'http://pop3he.cdn.intigral-ott.net/Titles/M0001881/M0001881_1.ism/manifest.mpd',
+  // 'https://akm.cdn.intigral-ott.net/Titles_HEVC/M0019324/M0019324_1.ism/manifest.mpd',
+  // 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd',
+  // 'https://akm.cdn.intigral-ott.net/Titles/S000854602003/S000854602003.ism/manifest.mpd',
+  // 'https://akm.cdn.intigral-ott.net/Titles_HEVC/M0020313/M0020313.ism/manifest.mpd',
+  // 'https://uselector.cdn.intigral-ott.net/Titles/M0020313/M0020313.ism/manifest.mpd',
+  // 'https://uselector.cdn.intigral-ott.net/Titles/M0020313/M0020313.ism/manifest.mpd',
+  // 'https://akm.cdn.intigral-ott.net/VOD/M0020313_CLR_HEVC/M0020313_CLR_HEVC.ism/manifest.mpd',
+  // 'https://akm.cdn.intigral-ott.net/VOD/M0020313_CLR_AVC/M0020313_CLR_AVC.ism/manifest.mpd',
+  // 'https://uselector.cdn.intigral-ott.net/POP2_HEVC/POP2_HEVC.isml/manifest.mpd',
+  // 'https://uselector.cdn.intigral-ott.net/VOD/M0019324_RTEST/M0019324_RTEST.ism/manifest.mpd',
+  // 'https://akm.cdn.intigral-ott.net/Titles_HEVC/M0019324/M0019324.ism/manifest.mpd',
+
+];
+
+const filter = '?filter=((type=="video"%26%26MaxHeight<=2160)%7C%7C(type=="audio")%7C%7C(type=="textstream"))&codec=hevc';
+
+let i = 0;
+let useFilter = false;
+
+const drmServerList = {
+  'com.widevine.alpha': 'https://wv.drm.intigral-ott.net:8063?deviceId=ZGI2MmY0ZTktYzVlMi0zMGM2LTg2YWMtMmNmMGE3YWE0ODA2',
+  // 'com.microsoft.playready': 'http://pr.drm.intigral-ott.net/PlayReady/RightsManager.asmx?deviceId=ZTcyODZjMTYtZGY5ZC0zYTc1LWI2ZTItNWMyYWJmZjhlYTE3',
+};
+
+const loadPlayer = (m=null) => {
+  const manifestUri = m || (uri[i] + (useFilter ? filter : ''));
+  document.getElementById('error').innerHTML = '';
+  document.getElementById('Quality').innerHTML = '';
+  document.getElementById('manifestUri').innerHTML = '';
+
+  document.getElementById('manifestUri').innerHTML = `#${i}: ${manifestUri}`;
+  console.log(manifestUri)
+  window.player.load(manifestUri).then((response) => {
+    console.log('Working', response);
+  }).catch((response) => {
+    document.getElementById('error').innerHTML = `Shaka Error : ${response.code}`;
+    console.log('Error', response);
+  });
+};
+
+window.loadPlayer = loadPlayer
+
+
+window.addEventListener('keydown', (e) => {
+  console.log('eee', e);
+  if (e.keyCode === 37) {
+    // if (useFilter) {
+    //   useFilter = false;
+    i -= 1;
+    i = i >= uri.length ? uri.length-1 : i;
+    i = i <= 0 ? 0 : i;
+  // } else {
+    useFilter = true;
+  // }
+  loadPlayer();
+  }
+  if (e.keyCode === 39) {
+    // if (useFilter) {
+    //   useFilter = false;
+      i += 1;
+      i = i >= uri.length ? 0 : i;
+    // } else {
+      useFilter = true;
+    // }
+    loadPlayer();
+  }
+  if (window.videoElement.paused) {
+    window.videoElement.play();
+  } else {
+    window.videoElement.pause();
+  }
+});
 
 function initApp() {
   // Install built-in polyfills to patch browser incompatibilities.
+  // eslint-disable-next-line no-undef
   shaka.polyfill.installAll();
+  // eslint-disable-next-line no-undef
   if (shaka.Player.isBrowserSupported()) {
     initPlayer();
   } else {
@@ -17,98 +85,113 @@ function initApp() {
     console.error('Browser not supported!');
   }
 }
-function initPlayer() {
-  var videoElement = document.getElementById('video');
-  var player = new shaka.Player();
-  window.ddd = videoElement;
-  window.addEventListener('keydown', function (e) {
-    console.log("eee", e)
-    if (videoElement.paused)
-      videoElement.play();
-    else
-      videoElement.pause();
-  });
-  
-  var drmServerList = {
-    'com.widevine.alpha': 'https://wv.drm.intigral-ott.net:8063?deviceId=ZWI0MmM1NzQtZTY4OS0zODEwLWE4NzEtMDViMDNlZTU2NWQ5',
-    'com.microsoft.playready': 'http://pr.drm.intigral-ott.net/PlayReady/RightsManager.asmx?deviceId=ZWI0MmM1NzQtZTY4OS0zODEwLWE4NzEtMDViMDNlZTU2NWQ5',
-  };
 
+const updateStats = () => {
+  const { player } = window;
+  const videoData = player.getVariantTracks();
+  const qualities = videoData.map((item) => [item.active ? '#' : '', `${item.language}`, `${item.height}p`, `${item.audioCodec}`, `${item.channelsCount}ch`].join(' '));
 
-  player.detach().then(function () {
-    player.attach(videoElement);
-    player.configure('abr.enabled', true);
-    player.configure({
-      drm: {
-        servers: drmServerList
-      }
-    });
-    document.getElementById("manifestUri").innerHTML = `URL : ${manifestUri}`;
-    player.load(manifestUri)
-      .then(function (response) {
-        console.log("Working", response);
-      })
-      .catch(function (response) {
-        document.getElementById("error").innerHTML = `Shaka Error : ${response.code}`;
-        console.log("Error", response);
-      });
+  // console.log('tracks', videoData);
+  // console.table(qualities);
 
-  });
-  player.addEventListener('error', onErrorEvent);
+  // const currentTrack = videoData.filter((tracks) => tracks.active);
+  const quality = qualities.join('<br/>');
+  document.getElementById('Quality').innerHTML = quality; // `Quality : ${currentTrack[0].height}p, ${currentTrack[0].channelsCount}Ch`;
 
-  
+  // console.log('videoData ', videoData);
+  // console.log('Status', player.getStats());
 
-  videoElement.onplaying = function () {
-    console.log("################### onplaying ####################");
-  }
+};
 
-  videoElement.oncanplay = function () {
-
-  }
-
-  videoElement.onloadeddata = function () {
-
-  console.log("################ on loaded data ##################" )
-   
-  console.log("tracks", player.getVariantTracks() );
-    let videoData = player.getVariantTracks();
-    let qualities = videoData.map((item)=> item.height)
-    console.log("qualities", qualities)
-    let currentTrack =  videoData.filter((tracks) => tracks.active);
-    document.getElementById("error").innerHTML = `Quality : ${currentTrack[0].height} x ${currentTrack[0].width}`;
-    console.log("active ", currentTrack[0])
-   
-    setTimeout(()=>{ 
-      const item =  videoData[0]
-      console.log("quality switch with", item)
-      player.configure('abr.enabled', false);
-      player.selectVariantTrack(item, true)
-     }, 15000)
-
-  }
- 
-  videoElement.ontimeupdate = function () {
-  console.log("################ time update ##################",  player.getStats())
-  }
-  
-  videoElement.onpause = function () {
-  console.log("################ on pausing ##################",  player.getStats())
-  }
-
-  videoElement.onloadedmetadata = function () {
-    console.log("################ onloadedmetadata ##################");
-    console.log("onloadedmetadata called", videoElement.currentTime);
-  };
+function onError(error) {
+  // Log the error.
+  console.error('Error code', error.code, 'object', error);
+  document.getElementById('error').innerHTML = `Shaka Error : ${error.code}`;
 }
 
 function onErrorEvent(event) {
   onError(event.detail);
 }
 
-function onError(error) {
-  // Log the error.
-  console.error('Error code', error.code, 'object', error);
-  document.getElementById("error").innerHTML = `Shaka Error : ${error.code}`;
+function initPlayer() {
+  const videoElement = document.getElementById('video');
+  // eslint-disable-next-line no-undef
+  const player = new shaka.Player();
+  shaka.log.setLevel(shaka.log.Level.V2);
+  window.videoElement = videoElement;
+  window.player = player;
+
+  player.attach(videoElement);
+  // player.configure('abr.enabled', true);
+  // player.configure('preferredLanguage', 'en');
+  // player.configure('preferredAudioChannelCount', 6);
+  // player.configure('preferredAudioCodecs', ['ac-3', 'ec-3'])
+  // player.configure('streaming.alwaysStreamText', true)
+
+  player.configure({
+    drm: {
+      servers: drmServerList,
+    },
+  });
+
+  loadPlayer();
+
+
+  const resposeFilter = (type, response) => {
+    if(type === 0 ) {
+      console.log('%%%% response type:', type, 'response:', response);
+      const manifestUri =  response.uri;
+      fetch(manifestUri).then(res=>{
+        res.text().then(manifest=>{
+
+          var parsedManifest = mpdParser.parse(manifest, { manifestUri });
+          console.log(parsedManifest)
+        });
+
+      });
+
+    }
+  };
+
+  // Debuging Shaka network engine
+  player.getNetworkingEngine().registerResponseFilter(resposeFilter);
+
+  player.addEventListener('error', onErrorEvent);
+
+  videoElement.onplaying = function () {
+    console.log('################### onplaying ####################');
+    updateStats();
+    console.log(player.getManifest())
+    console.log(player.getManifest())
+  };
+
+  videoElement.oncanplay = function () {
+    console.log('can play', player, videoElement);
+  };
+
+  videoElement.onloadeddata = function () {
+
+  };
+
+  videoElement.ontimeupdate = function () {
+    // console.log('################ time update ##################');
+    // console.log(player.getStats())
+    updateStats();
+  };
+
+  videoElement.onpause = function () {
+    console.log('################ on pausing ##################', player.getStats());
+  };
+
+  videoElement.onloadedmetadata = function () {
+    console.log('################ onloadedmetadata ##################');
+    console.log('onloadedmetadata called', videoElement.currentTime);
+  };
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
+// document.addEventListener('keydown', (e) => {
+//   if (videoElement) {
+//     videoElement.play();
+//   }
+// })
